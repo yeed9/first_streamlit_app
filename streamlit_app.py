f6695cc966a9299bec35c0d99cf1d5cc2cebc7f7
yeed9 <desmondyee@gmail.com>
2023-02-16 10:44:13 +1100

    Update streamlit_app.py
    Change desc

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
 # New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input('View our fruits list - Add your favourites?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
   else:
